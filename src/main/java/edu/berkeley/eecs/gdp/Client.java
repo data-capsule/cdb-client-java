6d16d108c796533cb5972df0086f68e901b563a9
Shubham Mishra <grapheo12@gmail.com>
2022-11-30 16:08:32 -0800

    [WIP] Write ahead logging

--- src/main/java/edu/berkeley/eecs/gdp/Client.java
+++ src/main/java/edu/berkeley/eecs/gdp/Client.java
@@ -7,6 +7,9 @@ import io.grpc.ManagedChannel;
 import edu.berkeley.eecs.gdp.NetworkExchangeGrpc.NetworkExchangeBlockingStub;
 import edu.berkeley.eecs.gdp.NetworkExchangeGrpc.NetworkExchangeStub;
 import io.grpc.stub.StreamObserver;
+
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
 import java.util.Iterator;
 import java.util.List;
 import java.util.Vector;
@@ -22,6 +25,10 @@ public class Client {
     private SynchronousQueue<PDU> sq_local;
     private String cdbName;
     private String myName;
+    private long ts;
+    private long sync_ts;
+
+    private byte[] current_hash;
 
     private Thread t_recv;
     private Thread t_send;
@@ -36,6 +43,13 @@ public class Client {
         this.sq_local = new SynchronousQueue<>();
         this.cdbName = cdbName;
         this.myName = myName;
+
+        this.ts = 0;
+        this.sync_ts = 0;
+
+        this.current_hash = new byte[32];
+
+        // TODO: Freshness request to reload ts and sync_ts.
     }
 
     public void Init() {
@@ -110,11 +124,11 @@ public class Client {
         }
     }
 
-    private List<ByteString> GenericRequest(List<ByteString> args) throws InterruptedException{
+    private List<ByteString> GenericRequest(List<ByteString> args, String fwdName) throws InterruptedException{
         PDU.Builder pdu_builder = PDU.newBuilder()
             .setOrigin(myName)
             .setSender(myName)
-            .addFwdNames(cdbName);
+            .addFwdNames(fwdName);
 
         for (ByteString arg: args){
             pdu_builder.addMsg(arg);
@@ -125,6 +139,10 @@ public class Client {
         PDU resp_pdu = sq_net.take();
 
         return resp_pdu.getMsgList();
+    }
+
+    private List<ByteString> GenericRequest(List<ByteString> args) throws InterruptedException{
+        return GenericRequest(args, cdbName);
     }
 
     public Pair<KV_Status, ByteString> Read(ByteString key){
@@ -146,11 +164,62 @@ public class Client {
         return new Pair<KV_Status,ByteString>(null, null);
     }
 
+    private void WalRequest(ByteString key, ByteString val) throws InterruptedException, NoSuchAlgorithmException {
+        Log wal_req = Log.newBuilder()
+            .setKey(key)
+            .setVal(val)
+            .setTimestamp(ts)
+            .setSyncRecordTimestamp(sync_ts).build();
+
+        ByteString s_wal_req = wal_req.toByteString();
+        MessageDigest sha256 = MessageDigest.getInstance("SHA-256");
+        byte[] payload_hash = sha256.digest(s_wal_req.toByteArray());
+
+        ts++;
+        CapsuleHeader ch = CapsuleHeader.newBuilder()
+            .setSender(1)
+            .setPrevHash(ByteString.copyFrom(current_hash))
+            .setHash(ByteString.copyFrom(payload_hash))
+            .setTimestamp(ts)
+            .setLastLogicalTimestamp(ts-1)
+            .setMsgType("EOE")
+            .setMsgLen(s_wal_req.size())
+            .setReplyAddr(myName)
+            .setAppMeta("wal")
+            .setVerified(true)
+            .build();
+        byte[] head_hash = sha256.digest(ch.toByteArray());
+
+        CapsulePDU cpdu = CapsulePDU.newBuilder()
+            .setHeader(ch)
+            .setHeaderHash(ByteString.copyFrom(head_hash))
+            .setSignature(val)
+            .setSignatureLen(0)             // TODO: Remove these garbage values for signature
+            .setPayloadInTransit(s_wal_req)
+            .build();
+
+        ByteString wal_pdu = cpdu.toByteString();
+        Vector<ByteString> v_wal_pdu = new Vector<>();
+        v_wal_pdu.add(wal_pdu);
+        List<ByteString> resp = GenericRequest(v_wal_pdu, "dc");
+        
+        // TODO: Check if WAL succeeded
+    }
+
     public Triplet<KV_Status, ByteString, ByteString> Write(ByteString key, ByteString val){
+        try {
+            WalRequest(key, val);
+        } catch (Exception e) {
+            System.err.println(e);
+            return new Triplet<KV_Status,ByteString,ByteString>(KV_Status.WRITE_FAIL, null, null);
+            
+        }
+
         List<ByteString> payload = new Vector<>();
         payload.add(ByteString.copyFromUtf8("WRITE"));
         payload.add(key);
         payload.add(val);
+        
         try{
             List<ByteString> resp = GenericRequest(payload);
             ByteString status = resp.get(0);
@@ -161,9 +230,8 @@ public class Client {
             }
         }catch(Exception e){
             System.err.println(e);
+            return new Triplet<KV_Status,ByteString,ByteString>(KV_Status.WRITE_FAIL, null, null);
         }
-
-        return new Triplet<KV_Status,ByteString,ByteString>(null, null, null);
 
     }
 }
